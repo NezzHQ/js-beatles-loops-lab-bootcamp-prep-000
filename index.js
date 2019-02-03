5d9447d84b02e94c3fd3d7f5564dd2fa990b58f5
Bens Hilaire <benshilaire@gmail.com>
2019-02-03 00:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var = [];
+  for (var i = 0; i < musicians.length; i ++) {
+    array.push(musicians[i] + ' plays' + instruments[i]);
+  }
+  re
+}
+
+
+
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
